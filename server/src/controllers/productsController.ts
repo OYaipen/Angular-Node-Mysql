e1ba13c347a17a98cbc32e6fa8cb07e9a78a3c08
Owen Yaipen <1999yaipen@gmail.com>
2019-12-03 13:52:35 -0500

    rest API CRUD

--- server/src/controllers/productsController.ts
+++ server/src/controllers/productsController.ts
@@ -1,20 +1,34 @@
 import { Request, Response } from "express";
 import pool from "../database";
 class ProductsController {
-    public list(req: Request, res: Response) {
-        res.json({ text: 'list products' });
+    public async list(req: Request, res: Response) {
+        const products = await pool.query('SELECT*FROM products');
+        console.log(products);
+        res.json(products);
     }
-    public getOne(req: Request, res: Response) {
-        res.json({ text: 'list product ' + req.params.id });
+    public async getOne(req: Request, res: Response): Promise<any> {
+        const { id } = req.params;
+        const products = await pool.query('SELECT*FROM products WHERE id=?', [id]);
+        if (products.length > 0) {
+            return res.json(products[0]);
+        } else {
+            res.status(404).json({ text: "The product doesn't exists" });
+        }
     }
-    public create(req: Request, res: Response) {
-        res.json({ text: 'Creating a product' })
+    public async create(req: Request, res: Response): Promise<void> {
+        await pool.query('INSERT INTO products set ?', [req.body]);
+        res.json({ message: 'Product Saved' })
+        console.log(req.body);
     }
-    public update(req: Request, res: Response) {
-        res.json({ text: 'update a product ' + req.params.id })
+    public async update(req: Request, res: Response): Promise<void> {
+        const { id } = req.params;
+        await pool.query('UPDATE products set ? WHERE id = ?', [req.body,id]);
+        res.json({ message: "Product Update" })
     }
-    public delete(req: Request, res: Response) {
-        res.json({ text: 'delete a product ' + req.params.id })
+    public async delete(req: Request, res: Response): Promise<void> {
+        const { id } = req.params;
+        await pool.query('DELETE FROM products WHERE id = ?', [id]);
+        res.json({ message: "Product Delete" })
     }
 }
 
